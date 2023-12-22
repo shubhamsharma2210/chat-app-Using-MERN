874225feb5c3083f95d0cacb7e1555e535fd7d9b
shubham sharma <shubhamarela2001@gmail.com>
2023-12-22 11:48:58 +0530

    hi

---
+++ public/src/components/ChatInput.jsx
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+import { BsEmojiSmileFill } from "react-icons/bs";
+import { IoMdSend } from "react-icons/io";
+import styled from "styled-components";
+import Picker from "emoji-picker-react";
+import { AiFillCamera } from "react-icons/ai";
+
+export default function ChatInput({ handleSendMsg }) {
+  const [msg, setMsg] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [sendImage, setSendImage] = useState(null);
+  const handleEmojiPickerhideShow = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
+  const handleEmojiClick = (event, emojiObject) => {
+    let message = msg;
+    message += emojiObject.emoji;
+    setMsg(message);
+  };
+
+  const sendChat = (event) => {
+    event.preventDefault();
+    if (msg.length > 0) {
+      handleSendMsg(msg);
+      setMsg("");
+    }
+  };
+
+  const handleImageshow = (event) => {
+    const file = event.target.files[0];
+    setSendImage(file);
+    console.log("image", handleImageshow);
+  };
+
+  return (
+    <Container>
+      <div className="button-container">
+        <div className="emoji">
+          <BsEmojiSmileFill onChange={handleEmojiPickerhideShow} />
+          <AiFillCamera onClick={handleImageshow} type="file" />
+
+          {sendImage && (
+            <img src={URL.createObjectURL(sendImage)} alt="selected" />
+          )}
+          {showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />}
+        </div>
+      </div>
+      <form className="input-container" onSubmit={(event) => sendChat(event)}>
+        <input
+          type="text"
+          placeholder="type your message here"
+          onChange={(e) => setMsg(e.target.value)}
+          value={msg}
+        />
+        <button type="submit">
+          <IoMdSend />
+        </button>
+      </form>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 5% 95%;
+  background-color: #080420;
+  padding: 0 2rem;
+  @media screen and (min-width: 720px) and (max-width: 1080px) {
+    padding: 0 1rem;
+    gap: 1rem;
+  }
+  .button-container {
+    display: flex;
+    align-items: center;
+    color: white;
+    gap: 0.2rem;
+    font-size: 1.5rem;
+
+    .emoji {
+      display: flex;
+      gap: 0.2rem;
+      position: relative;
+      margin-right: 40px;
+      svg {
+        font-size: 1.5rem;
+        color: #ffff00c8;
+        margin-right: -57px;
+        cursor: pointer;
+      }
+      .camera {
+        position: relative;
+        top: -300px;
+      }
+
+      .emoji-picker-react {
+        position: absolute;
+        top: -350px;
+        background-color: #080420;
+        box-shadow: 0 5px 10px #9a86f3;
+        border-color: #9a86f3;
+        .emoji-scroll-wrapper::-webkit-scrollbar {
+          background-color: #080420;
+          width: 5px;
+          &-thumb {
+            background-color: #9a86f3;
+          }
+        }
+        .emoji-categories {
+          button {
+            filter: contrast(0);
+          }
+        }
+        .emoji-search {
+          background-color: transparent;
+          border-color: green;
+        }
+        .emoji-group:before {
+          background-color: #080420;
+        }
+      }
+    }
+  }
+  .input-container {
+    width: 100%;
+    border-radius: 2rem;
+    display: flex;
+    align-items: center;
+    gap: 2rem;
+    background-color: #ffffff34;
+    input {
+      width: 90%;
+      height: 60%;
+      background-color: transparent;
+      color: white;
+      border: none;
+      padding-left: 1rem;
+      font-size: 1.2rem;
+
+      &::selection {
+        background-color: #9a86f3;
+      }
+      &:focus {
+        outline: none;
+      }
+    }
+    button {
+      padding: 0.3rem 2rem;
+      border-radius: 2rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: green;
+      border: none;
+      @media screen and (min-width: 720px) and (max-width: 1080px) {
+        padding: 0.3rem 1rem;
+        svg {
+          font-size: 1rem;
+        }
+      }
+      svg {
+        font-size: 2rem;
+        color: white;
+      }
+    }
+  }
+`;
---
+++ public/src/components/Contacts.jsx
@@ -0,0 +1,161 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Logo from "../assets/logo.svg";
+import { BiSolidLeaf } from "react-icons/bi";
+
+export default function Contacts({ contacts, changeChat }) {
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
+  const [currentSelected, setCurrentSelected] = useState(undefined);
+  useEffect(async () => {
+    const data = await JSON.parse(
+      localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+    );
+    setCurrentUserName(data.username);
+    setCurrentUserImage(data.avatarImage);
+  }, []);
+  const changeCurrentChat = (index, contact) => {
+    setCurrentSelected(index);
+    changeChat(contact);
+  };
+  return (
+    <>
+      {currentUserImage && currentUserImage && (
+        <Container>
+          <div className="brand">
+            <span className="logo">
+              <BiSolidLeaf />
+            </span>
+            <h3>DrCrop</h3>
+          </div>
+          <div className="contacts">
+            {contacts.map((contact, index) => {
+              return (
+                <div
+                  key={contact._id}
+                  className={`contact ${
+                    index === currentSelected ? "selected" : ""
+                  }`}
+                  onClick={() => changeCurrentChat(index, contact)}
+                >
+                  <div className="avatar">
+                    <img
+                      src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+                      alt=""
+                    />
+                  </div>
+                  <div className="username">
+                    <h3>{contact.username}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="current-user">
+            <div className="avatar">
+              <img
+                src={`data:image/svg+xml;base64,${currentUserImage}`}
+                alt="avatar"
+              />
+            </div>
+            <div className="username">
+              <h1>{currentUserName}</h1>
+            </div>
+          </div>
+        </Container>
+      )}
+    </>
+  );
+}
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 10% 75% 15%;
+  overflow: hidden;
+  background-color: #efe3bb;
+  color: black;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 0.2rem;
+    justify-content: center;
+    .logo {
+      font-size: 39px;
+      color: green;
+    }
+    h3 {
+      color: white;
+      font-size: 30px;
+      color: green;
+    }
+  }
+  .contacts {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow: auto;
+    gap: 0.8rem;
+    &::-webkit-scrollbar {
+      width: 0.2rem;
+      &-thumb {
+        background-color: #efe3bb;
+        width: 0.1rem;
+        border-radius: 1rem;
+      }
+    }
+    .contact {
+      background-color: #d3d3d3;
+      min-height: 5rem;
+      cursor: pointer;
+      width: 90%;
+      border-radius: 0.2rem;
+      border: 1px solid green;
+      padding: 0.4rem;
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      .avatar {
+        img {
+          height: 3rem;
+        }
+      }
+      .username {
+        h3 {
+          color: black;
+        }
+      }
+    }
+    .selected {
+      background-color: #9a86f3;
+    }
+  }
+
+  .current-user {
+    background-color: #efe3bb;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    .avatar {
+      img {
+        height: 4rem;
+        max-inline-size: 100%;
+      }
+    }
+    .username {
+      h1 {
+        color: black;
+        font-weight: 800;
+        font-size: 20px;
+      }
+    }
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      gap: 0.5rem;
+      .username {
+        h2 {
+          font-size: 1rem;
+        }
+      }
+    }
+  }
+`;
